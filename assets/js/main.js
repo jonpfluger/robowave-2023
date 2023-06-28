dfaebadf821d115d1a8618bef4dd0d36f71c4196
jonpfluger <jonpfluger718@gmail.com>
2023-06-27 21:08:16 -0500

    fullpage and typewriter setup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,21 @@
-console.log('Hello robo!')
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        licenseKey: 'gplv3-license',
+        navigation: true,
+        navigationTooltips: ['Welcome', 'New Robots', 'Used Robots', 'Contact']
+    });
+
+    // typewriterjs
+    var typewriterElements = document.querySelectorAll('.typewriter-effect')
+    for (var i = 0; i < typewriterElements.length; i++) {
+        var currentEl = typewriterElements[i]
+        var innerText = currentEl.innerText
+        new Typewriter(currentEl, {
+            loop: true,
+        })
+            .typeString(innerText)
+            .pauseFor(1000)
+            .start()
+    }
+});
+
